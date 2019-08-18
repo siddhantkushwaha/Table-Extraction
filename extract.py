43674d3f7718b7242ca1d5ed6504fc81cca69769
Siddhant Kushwaha <k16.siddhant@gmail.com>
2019-08-18 12:40:29 +0530

    improved add border code

--- extract.py
+++ extract.py
@@ -26,7 +26,8 @@ def extract(image):
 
         # add outer borders artificially, some images may not have outer borders
         # this will lead to outer columns being omitted
-        table_image = add_border_padding(table_image)
+        table_image = add_border_padding(table_image, w=(2, 2, 2, 4))
+        # cv.imwrite(f'out/table{i}.jpg', table_image)
 
         # find table joints, intersections for the warped table
         _, h, v = get_mask(table_image)
@@ -74,8 +75,7 @@ def extract(image):
                                cell[0] * mult:(cell[0] + cell[2]) * mult]
 
                 out_tables[-1][-1].append({'row': r, 'column': c, 'cell': cell_cropped})
-
-                cv.imwrite(f'out/cell-{i}-{r}-{c}.jpg', cell_cropped)
+                # cv.imwrite(f'out/cell-{i}-{r}-{c}.jpg', cell_cropped)
 
     return out_tables
 
