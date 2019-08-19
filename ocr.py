afef3092fa687717c412e3b45b8e8a2d4935b4a7
Siddhant Kushwaha <k16.siddhant@gmail.com>
2019-08-20 02:04:46 +0530

    optimized func

--- ocr.py
+++ ocr.py
@@ -1,5 +1,3 @@
-from io import StringIO
-
 import pytesseract as ts
 import cv2 as cv
 
@@ -7,8 +5,7 @@ import pandas as pd
 
 
 def get_image_data(image):
-    ts_data = ts.image_to_data(image)
-    df = pd.read_csv(StringIO(ts_data), sep='\t')
+    df = ts.image_to_data(image, output_type=ts.Output.DATAFRAME)
     return df
 
 
@@ -34,6 +31,7 @@ def ocr(image):
 
 if __name__ == '__main__':
     img = cv.imread('data/table0.jpg')
+
     ocr_data = ocr(img)
 
     for i, data in ocr_data.iterrows():
@@ -41,5 +39,7 @@ if __name__ == '__main__':
                      pt2=(data['left'] + data['width'], data['top'] + data['height']), color=(0, 255, 0), thickness=1)
         cv.putText(img=img, text=data['text'], color=(0, 0, 255), org=(data['left'], data['top']),
                    fontFace=cv.FONT_HERSHEY_PLAIN, fontScale=1)
+
+        print(data['text'])
 
     cv.imwrite('out/ocr_results.jpg', img)
