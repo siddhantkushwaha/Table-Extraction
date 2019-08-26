200f68e18a32cf511bb08915e6ac3c3a06fa425a
Siddhant Kushwaha <k16.siddhant@gmail.com>
2019-08-26 21:27:19 +0530

    change in main.py

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import cv2 as cv
 
 from extract import extract
 
-if __name__ == '__main__':
-    image = cv.imread("data/example1.jpg")
 
+def main(image):
     tables = extract(image)
     for table in tables:
         df = pd.DataFrame(table.data)
         print(df)
+
+
+if __name__ == '__main__':
+    image = cv.imread("data/example1.jpg")
+    main(image)
